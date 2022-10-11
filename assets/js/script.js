ec973ec8e6afe8893853d9c91b9d8694455d2bcd
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2022-10-11 18:33:36 +0700

    style: add ui version 1.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,44 @@ window.setTimeout(() => {
 window.setTimeout(() => {
     $('body').removeClass('overflow-hidden');
 }, 1000)
+
+$(function () {
+    setFixedHeader();
+
+    window.addEventListener("scroll", (event) => {
+        setFixedHeader()
+    });
+
+    function setFixedHeader() {
+        if (window.scrollY > 146 && $(window).width() > 767) {
+            $('#header').addClass('sticky');
+        } else {
+            $('#header').removeClass('sticky');
+        }
+    }
+    
+    window.setTimeout(() => {
+        $('#CLLoading').fadeOut();
+    }, 400)
+})
+$('#to_top').click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+})
+/* $(".why-blox3 .box button").click(function(){
+    $(".why-blox3 .box .collapse").removeClass("show");
+    $(".why-blox3 .box").removeClass("active");
+    
+    
+    if($(this).parent().hasClass("active")==false){
+        $(this).parent().addClass("active");
+        $(this).parent().find(".collapse").addClass("show");
+        
+    }
+    else{
+        $(this).parent().removeClass("active");
+        $(this).parent().find(".collapse").removeClass("show");
+    }
+}); */
--- assets/js/submit-email.js
+++ assets/js/submit-email.js
@@ -1,5 +1,13 @@
+$(document).ready(function() {
+    //$('#SubscribeModal').css('display', 'block'); 
+    //document.getElementById('SubscribeModal').show();
+    
+});
 document.getElementById("mc-embedded-subscribe").addEventListener("click", function() {
     var email = document.getElementById("mce-EMAIL").value;
+    /* $('#SubscribeModal').modal({
+        show: 'true'
+    });  */
     if(validateEmail(email)) {
         $.ajax({
             type: 'post',
@@ -17,9 +25,59 @@ document.getElementById("mc-embedded-subscribe").addEventListener("click", funct
             }
         });
         openVerifyInNewTab("mce-EMAIL");
+        bootstrap.Modal.getOrCreateInstance(document.getElementById('SubscribeModal')).show();
     }
 });
-
+document.getElementById("mc-embedded-subscribe1").addEventListener("click", function() {
+    var email = document.getElementById("mce-EMAIL1").value;
+    /* $('#SubscribeModal').modal({
+        show: 'true'
+    });  */
+    if(validateEmail(email)) {
+        $.ajax({
+            type: 'post',
+            url: 'https://subscribe.metabase.build',
+            data: JSON.stringify({
+                email: email,
+                project: 'Blox3'
+            }),
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                
+            },
+            error: function (err) {
+                
+            }
+        });
+        openVerifyInNewTab("mce-EMAIL1");
+        bootstrap.Modal.getOrCreateInstance(document.getElementById('SubscribeModal')).show();
+    }
+});
+document.getElementById("mc-embedded-subscribe2").addEventListener("click", function() {
+    var email = document.getElementById("mce-EMAIL2").value;
+    /* $('#SubscribeModal').modal({
+        show: 'true'
+    });  */
+    if(validateEmail(email)) {
+        $.ajax({
+            type: 'post',
+            url: 'https://subscribe.metabase.build',
+            data: JSON.stringify({
+                email: email,
+                project: 'Blox3'
+            }),
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                
+            },
+            error: function (err) {
+                
+            }
+        });
+        openVerifyInNewTab("mce-EMAIL2");
+        bootstrap.Modal.getOrCreateInstance(document.getElementById('SubscribeModal')).show();
+    }
+});
 function openVerifyInNewTab(inputId) {
     var email = document.getElementById(inputId).value;
     if (validateEmail(email)) {
