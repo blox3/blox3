bfcc76e04adfa2e1ec79d1669e8c542552127a93
Trong@codelight <trong@codelight.co>
2022-12-09 15:18:49 +0700

    add questions

--- assets/js/submit-email.js
+++ assets/js/submit-email.js
@@ -19,6 +19,22 @@ var ListQuestion = [
     {
         question: "How can I involve in Blox3 community?",
         answer: "The Blox3 community is open to everyone. Take the first step on your Blox3 journey by joining the <a href='https://discord.gg/4zUvcdK7DZ' target='_blank'>Official Blox3 Discord Community</a>"
+    },
+    {
+        question: "What if the borrowers cannot pay the monthly interest used to pay consumer investors?",
+        answer: "When the default event happens, Blox3 should be able to liquidate the collateral assets within a few days to the network of committed buyers. That information should be specified in the deal term before consumer investors deposit the money to join the deal. Blox3 platform will be transparent about what happens in each deal."
+    },
+    {
+        question: "Are things like FTX utilizing the fund of users for their investment applied to Blox3?",
+        answer: "Before joining our deal, the consumer investors must read the term carefully. In the term, we tell them explicitly when we start using the money to do business with local businesses on a specific date mentioned in the deal (to be exactly when the pool is closed) and when Blox3 gets back the interest monthly as well as the whole principle at the end of the deal."
+        + "<br/>" + "<br/>"
+        + "Given the current DeFi rate is low because of the unsustainable business model and the bear market, THE ONLY WAY to generate good yields is to utilize the fund to do business with real businesses. We will propose other scalable approaches to deal with real businesses in the near future."
+    },
+    {
+        question: "How does Blox3 deal with the countries' regulations?",
+        answer: "Digital assets held in custody are not guaranteed by Blox3 and are not FDIC-insured. Blox3 is not available for US persons or other prohibited jurisdictions."
+        + "<br/>" + "<br/>"
+        + "Most of the crypto-based project is not fully FDIC complied - crypto investors or consumer investors with stable coins have to take the risk to use our product. But as we mentioned: “Given the current DeFi rate is low because of the unsustainable business model and the bear market; THE ONLY WAY to generate good yields is to utilize the fund to do business with real businesses”."
     }
 ]
 $(document).ready(function () {
